bba2f346fc99d937faf4631c998f2940b9877ffe
Roy Selig <roy.selig@pubmatic.com>
2015-04-01 21:56:10 -0400

    adds carModel and CarModelYear services

--- app/app.js
+++ app/app.js
@@ -2,12 +2,10 @@
  * Created by RoySelig on 4/1/15.
  */
 
-(function() {
+(function () {
     angular.module( "app", [] )
 
         .factory( "CarPickerService", function ( $http ) {
-
-
 
             function getCarMakes( callback ) {
 
@@ -15,17 +13,49 @@
                     .success( callback );
             }
 
+            function getCarModels( makeFilter, callback ) {
+
+                $http.get( "data/car-models.json?filter=" + makeFilter )
+                    .success( function ( data ) {
+
+                        //we filter here for simplicity but it is actually the responsibility of the server
+                        var make = data.filter( function ( m ) {
+                            return m.name === makeFilter;
+                        } )
+
+                        var result = make[ 0 ];
+
+                        callback.call( this, result.models );
+                    }
+                );
+            }
+
+            function getCarModelYears( modelYearFilter, callback ) {
+
+                $http.get( "data/car-years.json?filter=" + modelYearFilter )
+                    .success( callback );
+
+            }
+
             return {
-                getCarMakes: getCarMakes
+                getCarMakes: getCarMakes,
+                getCarModels: getCarModels,
+                getCarModelYears: getCarModelYears
             }
 
         } )
 
-        .controller( "main", ["$scope", "CarPickerService", function ( $scope, CarPickerService ) {
+        .controller( "main", [ "$scope", "CarPickerService", function ( $scope, CarPickerService ) {
 
+            CarPickerService.getCarMakes( function ( result ) {
+                $scope.makes = result;
+            } );
+            CarPickerService.getCarModels( "Toyota", function ( result ) {
+                $scope.models = result;
+            } );
+            CarPickerService.getCarModelYears( "Toyota+Corolla", function ( result ) {
+                $scope.years = result;
+            } );
 
-
-            CarPickerService.getCarMakes( function( data ){ $scope.makes = data; });
-
-        } ]);
+        } ] );
 })();
